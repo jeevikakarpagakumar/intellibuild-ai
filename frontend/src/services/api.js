14073feb2ff82b4716c79d0ecef1b9f58313181d
Jeevika Kumar <jeevikakumar@Jeevikas-MacBook-Air.local>
2026-03-09 23:32:24 +0530

    made changes in backend main

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "https://fsws8w9yzf.execute-api.us-east-1.amazonaws.com/prod"
-});
-
 export const analyzeRepo = async (repoUrl) => {
-  const res = await API.post("/analyze", {
-    repo_url: repoUrl,
-    explanation_type: "structure",
-    abstraction_level: "developer"
-  });
+
+  const res = await axios.post(
+    "https://3.237.18.233/analyze",
+    {
+      repo_url: repoUrl,
+      explanation_type: "structure",
+      abstraction_level: "developer"
+    }
+  );
 
   return res.data;
 };
